0f3070b04b274efefe113d4b9498bf9a14eb5a91
AlexeyChekalin <sphinxsk@yandex.ru>
2021-06-11 15:09:08 +0300

    make lesson03

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
-var money = 100000; // Доход за месяц
-var income = 'фриланс'; // дополнительный доход
-var addExpenses = 'интернет, такси, коммуналка'; // дополнительные расходы
-var deposit = false;
-var mission = 1000000; // нужно накопить
-var period = 6; // количество месяцев
+'use strict';
+let money = 100000; // Доход за месяц
+let income = 'фриланс'; // дополнительный доход
+let addExpenses = 'интернет, такси, коммуналка'; // дополнительные расходы
+let deposit = false;
+let mission = 1000000; // нужно накопить
+let period = 6; // количество месяцев
 
-var budgetDay = money / 30;
+let budgetDay = money / 30;
 
 // alert('любой текст');
 // console.log('любой текст');
@@ -18,3 +19,30 @@ console.log('Период равен ' + period + ' месяцев.');
 console.log('Цель заработать ' + mission + ' рублей.');
 console.log(addExpenses.toLowerCase().split(', '));
 console.log(budgetDay);
+
+money = prompt('Ваш месячный доход?');
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую.');
+deposit = confirm('Есть ли у вас депозит в банке?');
+let expenses1 = prompt('Введите обязательную статью расходов?');
+let amount1 = prompt('Во сколько это обойдется?');
+let expenses2 = prompt('Введите обязательную статью расходов?');
+let amount2 = prompt('Во сколько это обойдется?');
+
+let budgetMonth = money - amount1 - amount2;
+
+console.log('Бюджет на месяц: ', budgetMonth);
+console.log('Цель будет дотигнута за ', Math.ceil(mission / budgetMonth), ' месяцев.');
+
+budgetDay = budgetMonth / 30;
+
+console.log('Бюджет на день: ', Math.floor(budgetDay));
+
+if (budgetDay > 1200) {
+  console.log('У вас высокий уровень дохода.');
+} else if (budgetDay > 600) {
+  console.log('У вас средний уровень дохода.');
+} else if (budgetDay >= 0) {
+  console.log('К сожалению, у вас уровень дохода ниже среднего.');
+} else {
+  console.log('Пора что-то менять в этой жизни!');
+}
